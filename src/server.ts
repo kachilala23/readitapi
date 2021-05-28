6dbf4c42ef0fb1a3b3e5cdfcc4600d3601baf6f0
kachilala <kachilala23@gmail.com>
2021-05-28 15:24:58 +0100

    login and validation with jwt cookie-parser implemented

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 import express from 'express'
 import morgan from 'morgan'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+dotenv.config()
 
 import authRoutes from './routes/auth'
 
@@ -12,17 +16,18 @@ const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(trim)
+app.use(cookieParser())
 
 app.get('/', (_, res) => res.send('Hello World'))
 app.use('/api/auth', authRoutes)
 
 app.listen(5000, async () => {
-    console.log('Server running at http://localhost:5000')
+  console.log('Server running at http://localhost:5000')
 
-    try {
-        await createConnection()
-        console.log('Database connected!')
-    } catch (err) {
-        console.log(err)
-    }
+  try {
+    await createConnection()
+    console.log('Database connected!')
+  } catch (err) {
+    console.log(err)
+  }
 })
